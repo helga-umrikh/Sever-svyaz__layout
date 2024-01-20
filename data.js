dfd7e68939a4880c63507400c7b7152aa34bcaf8
helga-umrikh <zetaumr@gmail.com>
2024-01-20 23:17:01 +0300

    feat: add dynamic cards for services section

---
+++ data.js
@@ -0,0 +1,25 @@
+const data = [
+  {
+    src: './assets/cardsData/img_1.png',
+    title: 'Городские камеры',
+    slogan: 'Будьте в курсе всех событий не выходя из дома',
+    price: 0,
+
+  },
+  {
+    src: './assets/cardsData/img_2.png',
+    title: 'Дворовые камеры',
+    slogan: 'Наблюдайте за вашим имуществом не выходя из дома через любое устройство',
+    price: 0,
+    
+  },
+  {
+    src: './assets/cardsData/img_3.png',
+    title: 'Персональные камеры',
+    slogan: 'Где бы вы ни находились, вы всегда можете узнать о происходящем дома с любого устройства',
+    price: 350,
+    
+  }
+]
+
+export default data;
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import data from './data.js'
+
 document.addEventListener('DOMContentLoaded', function () {
     const burgerBtn = document.querySelector('.header__burger-menu')
     const navigation = document.querySelector('.header__navigation')
@@ -9,4 +11,73 @@ document.addEventListener('DOMContentLoaded', function () {
             navigation && navigation.classList.toggle('is-active')
             clicked = !clicked
         })
+})
+
+function renderCards(data) {
+    const cardsContainer = document.querySelector('.services__cards-container')
+
+    data.forEach((item, index) => {
+        const card = document.createElement('div')
+        card.classList.add('card')
+        card.setAttribute('key', index)
+
+        const cardContainer = document.createElement('div')
+        cardContainer.classList.add('card__container')
+
+        const cardImage = document.createElement('img')
+        cardImage.classList.add('card__image')
+        cardImage.src = item.src
+        cardImage.alt = 'пример камеры'
+
+        const cardTitle = document.createElement('p')
+        cardTitle.classList.add('card__title')
+        cardTitle.innerText = item.title
+
+        const cardSlogan = document.createElement('p')
+        cardSlogan.classList.add('card__slogan')
+        cardSlogan.innerText = item.slogan
+
+        const bottomContainer = document.createElement('div')
+
+        const cardPrice = document.createElement('div')
+        cardPrice.classList.add('card__price')
+
+        const cardPriceNumber = document.createElement('p')
+        cardPriceNumber.classList.add('card__price-number')
+        cardPriceNumber.innerText = item.price
+
+        const priceTitleContainer = document.createElement('div')
+
+        const currencyTitle = document.createElement('p')
+        currencyTitle.innerText = 'руб'
+        const periodTitle = document.createElement('p')
+        periodTitle.innerText = 'мес'
+
+        const cardButton = document.createElement('button')
+        cardButton.classList.add('card__button');
+        cardButton.classList.add('button');
+        cardButton.innerText = 'Узнать подробнее'
+
+        cardsContainer.appendChild(card)
+        card.appendChild(cardContainer)
+        cardContainer.appendChild(cardImage)
+        cardContainer.appendChild(cardTitle)
+        cardContainer.appendChild(cardSlogan)
+        cardContainer.appendChild(bottomContainer)
+        bottomContainer.appendChild(cardPrice)
+        bottomContainer.appendChild(cardButton)
+        cardPrice.appendChild(cardPriceNumber)
+        cardPrice.appendChild(priceTitleContainer)
+        priceTitleContainer.appendChild(currencyTitle)
+        priceTitleContainer.appendChild(periodTitle)
+
+        console.log('cards renred')
+    })
+
+    console.log('rendering is finished')
+}
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    renderCards(data)
 })
